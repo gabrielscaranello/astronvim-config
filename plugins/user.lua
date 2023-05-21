f93d45de1490371242797db798fddce527703259
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-05-21 17:03:12 -0300

    add visual multi

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,22 +1,6 @@
 return {
   {
-    "onsails/lspkind.nvim",
-    opts = function(_, opts)
-      -- use codicons preset
-      opts.preset = "codicons"
-      -- set some missing symbol types
-      opts.symbol_map = {
-        Array = "",
-        Boolean = "",
-        Key = "",
-        Namespace = "",
-        Null = "",
-        Number = "",
-        Object = "",
-        Package = "",
-        String = "",
-      }
-      return opts
-    end,
+    "mg979/vim-visual-multi",
+    event = "User AstroFile",
   },
 }
