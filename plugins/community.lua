c12976ac6782d345b95d942addc62b261bede78f
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-05-21 16:25:23 -0300

    add spectre to find and replace in all project files

--- plugins/community.lua
+++ plugins/community.lua
@@ -8,12 +8,13 @@ return {
   { import = "astrocommunity.completion.codeium-vim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.cutlass-nvim" },
-  { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.editing-support.dial-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.project.nvim-spectre" },
   { import = "astrocommunity.utility.noice-nvim" },
 
   {
