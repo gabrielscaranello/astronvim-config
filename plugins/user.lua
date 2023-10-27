c6ec3d7e75789a0b0c2819a61fe51760157c85f7
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-10-27 15:00:09 -0300

    add coverage keys

--- plugins/user.lua
+++ plugins/user.lua
@@ -4,4 +4,35 @@ return {
     event = "VeryLazy",
     config = function() require("stay-centered").setup() end,
   },
+  {
+    "andythigpen/nvim-coverage",
+
+    keys = {
+      {
+        "<leader>lc",
+        function()
+          local coverage = require "coverage"
+
+          if vim.g.coverage_showing then
+            coverage.hide()
+          else
+            coverage.load(true)
+            coverage.show()
+          end
+
+          vim.g.coverage_showing = not vim.g.coverage_showing
+        end,
+        desc = "Coverage",
+      },
+      {
+        "<leader>lC",
+        function()
+          local coverage = require "coverage"
+          if not vim.g.coverage_showing then coverage.load(false) end
+          coverage.summary()
+        end,
+        desc = "Coverage Summary",
+      },
+    },
+  },
 }
