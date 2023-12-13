7dc130fdffd47b85d2db031411102085fdc9c1f7
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-12-13 14:58:59 -0300

    update configs

--- plugins/core.lua
+++ plugins/core.lua
@@ -61,8 +61,8 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       window = {
-        width = 35,
-        position = "right",
+        width = 40,
+        position = "left",
       },
     },
   },
